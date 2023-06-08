49a0f265395bdb23059cfa48e98ae3056e093df6
Rizky Junian Nugroho <rizky.j.n@mail.ugm.ac.id>
2023-06-08 07:02:00 +0700

    final deployment

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -2,20 +2,41 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import CircleProgress from "./CircleProgress";
 
-export default function Result() {
-  const [targetProgress, setTargetProgress] = useState(70);
+export default function Result({ result, handleSeekAgain }) {
+  const EXPRESS_URL = process.env.REACT_APP_EXPRESS;
 
-  const handleInputChange = (event) => {
-    setTargetProgress(Number(event.target.value));
-  };
+  const [targetProgress, setTargetProgress] = useState(
+    result.detection_result[0] * 100
+  );
+  const [id, setId] = useState("");
 
-  const handleShareClick = () => {
+  const handleShareClick = async () => {
+    console.log(result);
+    const response = await fetch(`${EXPRESS_URL}/api/fake`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        content: result.content,
+        detection_result: result.detection_result[0],
+      }),
+    });
+    if (response) {
+      const responseData = await response.json();
+      console.log(responseData);
+      setId(responseData._id);
+      setId(responseData._id);
+    } else if (!response) {
+      alert("notfound");
+    }
+
     if (navigator.share) {
       navigator
         .share({
-          title: "Share Title",
-          text: "Check out this website!",
-          url: "https://www.example.com",
+          title: "The Truth Has Been Revealed!",
+          text: "I have checked that this news is fake or not. Go check it out!",
+          url: `/result/${id}`,
         })
         .then(() => console.log("Share successful."))
         .catch((error) => console.error("Error sharing:", error));
@@ -29,31 +50,29 @@ export default function Result() {
       <div className="mb-12 lg:mb-0 lg:mt-2 lg:mr-24">
         <CircleProgress targetProgress={targetProgress} />
       </div>
-      <div className="flex flex-col gap-8">
+      <div className="flex flex-col gap-6">
         <div>
-          <input type="text" onChange={handleInputChange}></input>
           <h1 className="lg:text-left text-center text-2xl font-bold text-purple-900">
-            Do we help you a lot?
+            Here comes the truth!
           </h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-            cursus.
+          <p className="px-8 lg:px-0 text-center lg:text-left">
+            The percentage is the truth of the information you entered.
           </p>
         </div>
 
-        <div className="flex flex-row gap-4">
+        <div className="flex flex-col md:flex-row gap-4 justify-center items-center lg:justify-start lg:items-start">
           <Link
             to=""
             className=" items-center justify-center px-12 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-purple-600 hover:bg-purple-700"
           >
             Give Feedback
           </Link>
-          <Link
-            to="/seek"
+          <button
+            onClick={handleSeekAgain}
             className=" items-center justify-center px-12 py-2 rounded-md shadow-sm text-base font-medium text-purple-700 hover:text-white border border-purple-700 hover:bg-purple-700"
           >
             Seek Again
-          </Link>
+          </button>
           <Link
             onClick={handleShareClick}
             className=" items-center justify-center px-4 py-2 rounded-md shadow-sm text-base font-medium text-purple-700 hover:text-white border border-purple-700 hover:bg-purple-700"
--- src/pages/seekPage.jsx
+++ src/pages/seekPage.jsx
@@ -1,16 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import Seek from "../components/Seek";
+import Result from "../components/Result";
 import Contact from "../components/Contact";
 
-function seekPage() {
+function SeekPage() {
+  const FLASK_URL = process.env.REACT_APP_FLASK;
+
+  const [result, setResult] = useState(null);
+  const [error, setError] = useState(0);
+
+  const handleSubmit = async (e, inputText) => {
+    e.preventDefault();
+
+    if (inputText.length > 3000) {
+      setError(1); // Set error flag
+      return;
+    } else if (inputText === "") {
+      setError(2);
+      return;
+    } else setError(false);
+
+    try {
+      const response = await fetch(`${FLASK_URL}/detect-news`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ input_text: inputText }),
+      });
+
+      if (response.ok) {
+        // Handle successful response
+        const data = await response.json();
+        console.log(data);
+        setResult(data);
+      } else {
+        // Handle error response
+        console.error("Error:", response.statusText);
+      }
+    } catch (error) {
+      // Handle network error
+      console.error("Error:", error.message);
+    }
+  };
+
+  const handleSeekAgain = () => {
+    // Do something with the parameter received from the child component
+    setResult("");
+  };
+
   return (
     <div>
       <Navbar />
-      <Seek />
+      {result ? (
+        <Result result={result} handleSeekAgain={handleSeekAgain} />
+      ) : (
+        <Seek handleSubmit={handleSubmit} error={error} />
+      )}
+
       <Contact />
     </div>
   );
 }
 
-export default seekPage;
+export default SeekPage;
